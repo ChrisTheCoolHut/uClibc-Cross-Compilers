2335d97109454cde45ba433a92a0d81c55d257ac
Chris <chris_m_roberts@hotmail.com>
2019-05-15 23:58:39 -0400

    added compilers

--- arm-linux-uclibc-gnueabi/lib/gcc/arm-buildroot-linux-uclibcgnueabi/4.9.4/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "./configure --prefix=/home/caffix/tools/buildroot/output/host --sysconfdir=/home/caffix/tools/buildroot/output/host/etc --enable-static --target=arm-buildroot-linux-uclibcgnueabi --with-sysroot=/home/caffix/tools/buildroot/output/host/arm-buildroot-linux-uclibcgnueabi/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --with-gmp=/home/caffix/tools/buildroot/output/host --with-mpc=/home/caffix/tools/buildroot/output/host --with-mpfr=/home/caffix/tools/buildroot/output/host --with-pkgversion='Buildroot 2019.05-git-00671-g67ab484206' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --disable-libsanitizer --enable-tls --disable-libmudflap --enable-threads --without-isl --without-cloog --with-float=soft --with-abi=aapcs-linux --with-cpu=arm926ej-s --with-float=soft --with-mode=arm --enable-languages=c,c++ --with-build-time-tools=/home/caffix/tools/buildroot/output/host/arm-buildroot-linux-uclibcgnueabi/bin --enable-shared --disable-libgomp";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "arm926ej-s" }, { "float", "soft" }, { "mode", "arm" }, { "tls", "gnu" } };
--- mipsel-linux-uclibc/lib/gcc/mipsel-buildroot-linux-uclibc/4.9.4/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "./configure --prefix=/home/caffix/tools/buildroot/output/host --sysconfdir=/home/caffix/tools/buildroot/output/host/etc --enable-static --target=mipsel-buildroot-linux-uclibc --with-sysroot=/home/caffix/tools/buildroot/output/host/mipsel-buildroot-linux-uclibc/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --with-gmp=/home/caffix/tools/buildroot/output/host --with-mpc=/home/caffix/tools/buildroot/output/host --with-mpfr=/home/caffix/tools/buildroot/output/host --with-pkgversion='Buildroot 2019.05-git-00671-g67ab484206' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --disable-libsanitizer --enable-tls --disable-libmudflap --enable-threads --without-isl --without-cloog --with-float=soft --with-arch=mips32 --with-abi=32 --with-nan=legacy --enable-languages=c,c++ --with-build-time-tools=/home/caffix/tools/buildroot/output/host/mipsel-buildroot-linux-uclibc/bin --enable-shared --disable-libgomp";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "abi", "32" }, { "arch", "mips32" }, { "float", "soft" }, { "nan", "legacy" }, { "llsc", "llsc" } };
